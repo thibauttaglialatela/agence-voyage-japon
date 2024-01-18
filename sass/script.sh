bcccce130bc398c0a1dc388d95de457b16bf36ba
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-01-18 12:19:23 +0100

    initialisation du projet

---
+++ sass/script.sh
@@ -0,0 +1,7 @@
+touch main.scss
+mkdir abstract pages layout base components
+cd abstract
+touch _function.scss _mixins.scss _variables.scss
+cd ..
+cd base
+touch _animations.scss _typography.scss _utilities.scss _base.scss
